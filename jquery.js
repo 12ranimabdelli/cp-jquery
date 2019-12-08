24ba8206b497dc7c52b6c0ea68dddf88b6681f26
12ranimabdelli <56515356+12ranimabdelli@users.noreply.github.com>
2019-12-08 17:40:06 +0100

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,13 @@
+$(".image-hover").mouseenter(function(){
+    $(this).find(".bt").show();
+    $(this).css("opacity","0.7");
+})
+$(".image-hover").mouseleave(function(){
+    $(this).find(".bt").hide();
+    $(this).css("opacity","1");
+
+})
+
+$("#red").click(function(){
+   alert(window.getSelection().getRangeAt(0))
+})
---
+++ js/main.js
@@ -0,0 +1,46 @@
+var text = document.getElementById("textArea");
+
+function bold(){
+    text.style.fontWeight = text.style.fontWeight=="bold"? "normal" : "bold";
+}
+
+
+function italic(){
+    if(text.style.fontStyle=="italic"){
+        text.style.fontStyle= "normal"
+    }else{
+        text.style.fontStyle="italic";
+    }
+}
+
+function underline(){
+    if(text.style.textDecoration=="underline"){
+        text.style.textDecoration= "none"
+    }else{
+        text.style.textDecoration="underline";
+    }
+}
+
+
+document.getElementById('selectSize').addEventListener("change",function(){
+    text.style.fontSize= document.getElementById('selectSize').value;
+})
+
+document.getElementById('selectFamily').addEventListener("change",function(){
+    text.style.fontFamily= this.value;
+})
+
+
+$(".image-hover").mouseenter(function(){
+    $(this).find(".bt").show();
+    $(this).css("opacity","0.7");
+})
+$(".image-hover").mouseleave(function(){
+    $(this).find(".bt").hide();
+    $(this).css("opacity","1");
+
+})
+
+$("#red").click(function(){
+   alert(window.getSelection().getRangeAt(0))
+})
